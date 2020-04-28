7f2141a0198609e457eaa9a1a9fd2803ede8696f
satyam sheel <sheelsatyam@ymail.com>
2020-04-28 16:14:09 +0530

    edit text connected to database

--- app/src/main/java/com/example/uoodma/editProfile.java
+++ app/src/main/java/com/example/uoodma/editProfile.java
@@ -2,13 +2,21 @@ package com.example.uoodma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.example.uoodma.login_register.phoneVerificationActivity;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class editProfile extends AppCompatActivity {
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
@@ -19,13 +27,10 @@ public class editProfile extends AppCompatActivity {
     Button editProfileSaveChanges;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
-
-        documentReference = db.collection("User").document(mAuth.getCurrentUser().getUid());
 
         editProfileEmailText = findViewById(R.id.editProfileEmailText);
         editProfileFullNameText = findViewById(R.id.editProfileFullNameText);
@@ -36,11 +41,29 @@ public class editProfile extends AppCompatActivity {
         editProfileStateText = findViewById(R.id.editProfileStateText);
         editProfilePinCodeText = findViewById(R.id.editProfilePinCodeText);
         editProfileSaveChanges = findViewById(R.id.editProfileSaveChanges);
-    }
 
-    @Override
-    protected void onStart() {
 
-        super.onStart();
+        editProfileSaveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userId = mAuth.getCurrentUser().getUid();
+                DocumentReference documentReference = db.collection("Users").document(userId);
+                Map<String, Object> user = new HashMap<>();
+                user.put("Alternate Phone", editProfileAlternatePhoneNumberText.getText().toString());
+                user.put("Full Address", fullAddressText.getText().toString());
+                user.put("City Name", editProfileCityText.getText().toString());
+                user.put("State Name", editProfileStateText.getText().toString());
+                user.put("Pin Code", editProfilePinCodeText.getText().toString());
+                documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(editProfile.this, "Sucess", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
     }
 }
+
+
+
